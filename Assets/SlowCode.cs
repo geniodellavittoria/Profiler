9343b933c5088fb548aa828b2955498411c06f2d
Tim Bolzern <t.bolzern@gmail.com>
2018-11-29 20:28:14 +0100

    added SlowCode

--- Assets/SlowCode.cs
+++ Assets/SlowCode.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class SlowCode : MonoBehaviour {
 
+    float timeValueRed;
+    float timeValueGreen;
+    float timeValueBlue;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +14,35 @@ public class SlowCode : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        GetComponent<Renderer>().material.color = ColorForTime();
+        DoLoop();
+        DoSomethingElse();
 	}
+
+    void DoLoop()
+    {
+        for(int i =0; i< 1000; i++)
+        {
+            GetComponent<Renderer>().material.color = ColorForTime();
+        }
+    }
+
+    void DoSomethingElse()
+    {
+        Debug.Log("look how slow logging is");
+    }
+
+    private Color ColorForTime()
+    {
+        timeValueRed += Time.deltaTime;
+        if (timeValueRed > 1f)
+            timeValueRed -= 1f;
+        timeValueGreen += Time.deltaTime * 2f;
+        if (timeValueGreen > 1f)
+            timeValueGreen -= 1f;
+        timeValueBlue = Time.deltaTime * 3f;
+        if (timeValueBlue > 1f)
+            timeValueBlue -= 1f;
+        return new Color(timeValueRed, timeValueGreen, timeValueBlue);
+    }
 }
